bff42e181d566b7dcd559666dc00af7c75fdefb4
karchx <kenethra.74@gmail.com>
2026-01-05 04:54:01 -0600

    feat: repl v1

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,36 @@ pub fn main() !void {
     defer _ = gpa.deinit();
 
     const allocator = gpa.allocator();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    try transport.initServer(allocator);
+    if (args.len > 1 and std.mem.eql(u8, args[1], "connect")) {
+        try repl();
+    } else {
+        try transport.initServer(allocator);
+    }
+
+}
+
+pub fn repl() !void {
+    var stdout_buf: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
+    const stdout = &stdout_writer.interface;
+
+    var stdin_buf: [1024]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buf);
+    const stdin = &stdin_reader.interface;
+
+    try print("> ", stdout);
+
+    while (stdin.takeDelimiterExclusive('\n')) | line| {
+        try print(line, stdout);
+    } else |err| {
+        return err;
+    }
+}
+
+fn print(msg: []const u8, writer: *std.Io.Writer) !void {
+    try writer.print("{s}", .{ msg });
+    try writer.flush();
 }
